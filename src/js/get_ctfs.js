8eb493427fe7c99c7223c1a799149828b5a37e22
dherediat <dherediat97@gmail.com>
2025-12-26 20:00:33 +0100

    feature: add platform name, add author ctf blog

--- src/js/get_ctfs.js
+++ src/js/get_ctfs.js
@@ -35,20 +35,53 @@ function getCtfs() {
                     const writeUpCard = document.createElement('div');
                     writeUpCard.classList.add('ctf-writeup-card');
                     writeUpCard.innerHTML = `
-                    <img class="ctf-writeup-img" src="${ctf.img_url}" alt="CTF Image"/>
+                    <img class="ctf-writeup-img" src="${
+                        ctf.img_url
+                    }" alt="CTF Image"/>
                     <div class="ctf-details">
                     <div class="ctf-title">${ctf.title}</div>
-                    <div class="ctf-level">Dificultad: <span class="ctf-level-${ctf.level}">${ctf.level}</span></div>
-                    <div class="ctf-category">Categorías: ${ctf.main_category}, ${ctf.tags}</div>
-                    <div class="ctf-platform">Plataforma: <a href="${ctf.platform}" target="_blank">${ctf.platform}</a></div>
-                    <div class="ctf-author">Autor: ${ctf.author}</div>
-                    <div class="ctf-write-up"><a href="${ctf.url}" class="show-write-up-title" target="_self"></a></div> 
+                    <div class="ctf-level">Dificultad: <span class="ctf-level-${
+                        ctf.level
+                    }">${ctf.level}</span></div>
+                    <div class="ctf-category">Categorías: ${
+                        ctf.main_category
+                    },${ctf.tags}</div>
+                    <div class="ctf-platform">Plataforma: <a href="${
+                        ctf.platform
+                    }" target="_blank">${getPlatformName(
+                        ctf.platform
+                    )}</a></div>
+                    <div class="ctf-author">Autor: <a href="${getAuthorBlogUrl(
+                        ctf.author
+                    )}" target="_blank">${ctf.author}</a></div>
+                    <div class="ctf-write-up"><a href="${
+                        ctf.url
+                    }" class="show-write-up-title" target="_self"></a></div> 
                     </div>
                 `;
                     ctfContainer.appendChild(writeUpCard);
                 });
             });
         });
+}
+
+function getPlatformName(platformUrl) {
+    const platformNames = {
+        'https://www.hackthebox.com/': 'Hack The Box',
+        'https://www.tryhackme.com/': 'TryHackMe',
+        'https://www.cyberdefenders.org/': 'CyberDefenders',
+        'https://labs.thehackerslabs.com/': 'TheHackerLabs',
+    };
+    return platformNames[platformUrl];
+}
+
+function getAuthorBlogUrl(authorName) {
+    const authorBlogUrls = {
+        Oscar: 'https://oscarai.tech/',
+        'condor & CuriosidadesDeHackers': 'https://curiosidadesdehackers.com/',
+        'El Pingüino de Mario': 'https://maalfer.github.io/whoami/',
+    };
+    return authorBlogUrls[authorName] || '#';
 }
 
 function groupByCategory(items, categories) {
