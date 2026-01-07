e10894dd00a15955fead4c905d2d2769ed4f127e
dherediat <dherediat97@gmail.com>
2026-01-07 13:34:43 +0100

    feature: add meta image, fix generic variable name - write-up.js

--- src/js/write-up.js
+++ src/js/write-up.js
@@ -1,8 +1,8 @@
-const writeUpParams = document.location.hash.split('#');
+const writeUpName = document.location.hash.split('#')[1];
 const ctfNameElement = document.querySelector('.ctfName');
 const ctfName =
-    writeUpParams[1].replace(/_/g, ' ').charAt(0).toUpperCase() +
-    writeUpParams[1].replace(/_/g, ' ').slice(1);
+    writeUpName.replace(/_/g, ' ').charAt(0).toUpperCase() +
+    writeUpName.replace(/_/g, ' ').slice(1);
 
 ctfNameElement.textContent = ctfName.charAt(0).toUpperCase() + ctfName.slice(1);
 
@@ -11,16 +11,12 @@ const categoryNavElement = document.querySelector('.category');
 
 var md = new MobileDetect(navigator.userAgent);
 
-AsciinemaPlayer.create(
-    `assets/write-ups/${writeUpParams[1]}.cast`,
-    writeUpDiv,
-    {
-        rows: md.os() == 'iOS' || md.os() == 'AndroidOS' ? '60' : 20,
-        idleTimeLimit: 2,
-        controls: true,
-        autoPlay: true,
-    }
-);
+AsciinemaPlayer.create(`assets/write-ups/${writeUpName}.cast`, writeUpDiv, {
+    rows: md.os() == 'iOS' || md.os() == 'AndroidOS' ? '60' : 20,
+    idleTimeLimit: 2,
+    controls: true,
+    autoPlay: true,
+});
 
 const bibliography = document.querySelector('.bibliography-links');
 
@@ -30,7 +26,7 @@ fetch(CONFIG_URL_WRITE_UP)
     })
     .then((ctfList) => {
         const ctf = ctfList.filter((ctfFound) =>
-            ctfFound.writeup_url.includes(writeUpParams[1])
+            ctfFound.writeup_url.includes(writeUpName)
         )[0];
         if (ctf.bibliography_links) {
             for (let i = 0; i < ctf.bibliography_links.length; i++) {
