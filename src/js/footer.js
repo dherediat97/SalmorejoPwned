3fd9a6fba2284c16974ad7286d3648dbab56fdd6
dherediat <dherediat97@gmail.com>
2026-02-05 10:59:17 +0100

    feature: move translations

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,6 +1,6 @@
 const footer = document.getElementById('footer');
 footer.innerHTML = `
     <footer class="container">
-        <p><span class="made-with-title">Made with</span> ❤️ <span class="by-title">by</span> <a href="http://github.com/dherediat97">@dherediat97</a></p>
+        <p><span class="made-with-title"></span> ❤️ <span class="by-title"></span> <a href="http://github.com/dherediat97">@dherediat97</a></p>
     </footer>
 `;
